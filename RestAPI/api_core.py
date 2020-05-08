3dd25f98add4fefe60113e2550b28b76bfb459cb
b0czek <dariusz.majnert@gmail.com>
2020-05-08 17:04:39 +0200

    completed api functionality(i hope) and working 'round docker

--- RestAPI/api_core.py
+++ RestAPI/api_core.py
@@ -1,54 +1,68 @@
 import ipcalc
 import json
-ip_addr = '172.0.0.22/16'
-subnets_to_divide = [16384, 5000, 120]
-divide_as_hosts = False
-def getClosestPowerOfTwo(number, as_hosts, poweroftwo=0):
-    if as_hosts:
-        if number + 2 >= 2**poweroftwo:
-            return getClosestPowerOfTwo(number, divide_as_hosts, poweroftwo+1)
+
+class Divider:
+    @staticmethod
+    def __getClosestPowerOfTwo(number, as_hosts, poweroftwo=0):
+        if as_hosts:
+            if number + 2 > 2**poweroftwo:
+                return Divider.__getClosestPowerOfTwo(number, as_hosts, poweroftwo+1)
+            else:
+                return poweroftwo
         else:
-            return poweroftwo
-    else:
-        if number > 2**poweroftwo:
-            return getClosestPowerOfTwo(number, divide_as_hosts, poweroftwo+1)
-        else:
-            return poweroftwo
+            if number > 2**poweroftwo:
+                return Divider.__getClosestPowerOfTwo(number, as_hosts, poweroftwo+1)
+            else:
+                return poweroftwo
+
+    @staticmethod
+    def __listNetworkParameters(addr, subnet):
+        data = dict()
+        network = ipcalc.Network(addr, subnet)
+        data['network_address'] = str(network.network())
+        data['network_address_bin'] = network.network().bin()
+        data['subnet_mask'] = str(network.netmask())
+        data['subnet_mask_bin'] = network.netmask().bin()
+        data['subnet_mask_prefix'] = network.mask
+        data['broadcast_address'] = str(network.broadcast())
+        data['broadcast_address_bin'] = network.broadcast().bin()
+        data['first_host_address'] = str(network.host_first())
+        data['first_host_address_bin'] = network.host_first().bin()
+        data['last_host_address'] = str(network.host_last())
+        data['last_host_address_bin'] = network.host_last().bin()  
+        data['addresses_count'] = network.size()      
+        return data, network
 
 
+    @staticmethod
+    def divide(ip: str, cidr:int, subnets: list, divide_as_hosts: bool):
+        x = dict()
+        try:
+            main_network_parameters, main_network = Divider.__listNetworkParameters(ip,cidr)
+            x['main_network'] = main_network_parameters
+            operating_subnet_address = main_network.network()
+            x['subnets'] = list()
+            x['error'] = None
+        except ValueError as e:
+            x['error'] = str(e)
+            return x
+        for subnet in sorted(subnets, reverse=True):
+            try:
+                host_bits = Divider.__getClosestPowerOfTwo(subnet, divide_as_hosts)
+                subnet_prefix = 32 - host_bits
+                params, subnetwork = Divider.__listNetworkParameters(str(operating_subnet_address), subnet_prefix)
+                if main_network.broadcast() < subnetwork.broadcast():
+                    raise ValueError('Główna sieć nie pomieści tyle adresów z tej podsieci, pomijanie')
+                elif subnet > 2**32-cidr:
+                    raise ValueError('Liczba adresów w głównej sieci jest mniejsza od liczby adresów w podsieci, pomijanie')
+                elif main_network.network() > subnetwork.broadcast() or main_network.network() > subnetwork.network():
+                    raise ValueError('Protokół IPv4 nie pomieści tylu adresów, IP overflow, pomijanie') 
+                else:
+                    params['error'] = None
+                    operating_subnet_address = subnetwork.broadcast() + 1
+                params['initial_addresses_count'] = subnet+2 if divide_as_hosts else subnet
+                x['subnets'].append(params)
 
-def main():
-    x = dict()
-    ip, subnet = ip_addr.split('/')
-    main_network_parameters, main_network = listNetworkParameters(ip,subnet)
-    x['main_network'] = main_network_parameters
-    operating_subnet_address = main_network.network()
-    x['subnets'] = dict()
-    for subnet in sorted(subnets_to_divide, reverse=True):
-        host_bits = getClosestPowerOfTwo(subnet, divide_as_hosts)
-        subnet_prefix = 32 - host_bits
-        params, subnetwork = listNetworkParameters(str(operating_subnet_address), subnet_prefix)
-        x['subnets'][str(subnet)] = params
-        operating_subnet_address = subnetwork.broadcast() + 1
-
-    print(json.dumps(x))
-
-def listNetworkParameters(addr, subnet):
-    data = dict()
-    network = ipcalc.Network(addr, subnet)
-    data['network_address'] = str(network.network())
-    data['network_address_bin'] = network.network().bin()
-    data['subnet_mask'] = str(network.netmask())
-    data['subnet_mask_bin'] = network.netmask().bin()
-    data['subnet_mask_prefix'] = network.mask
-    data['broadcast_address'] = str(network.broadcast())
-    data['broadcast_address_bin'] = network.broadcast().bin()
-    data['first_host_address'] = str(network.host_first())
-    data['first_host_address_bin'] = network.host_first().bin()
-    data['last_host_address'] = str(network.host_last())
-    data['last_host_address_bin'] = network.host_last().bin()  
-    data['addresses_count'] = network.size()      
-    return data, network
-
-if __name__ == '__main__':
-    main()
+            except ValueError as e:
+                x['subnets'].append(dict(initial_addresses_count=subnet, error=str(e)))
+        return x
--- RestAPI/run.py
+++ RestAPI/run.py
@@ -1,11 +1,7 @@
 from flask import Flask
-
+from api_core import Divider
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "chuj"
-
-
-
-app.run(host='0.0.0.0', port=80, debug=True)
+	return Divider.divide('172.1.1.1', 24, [100, 100], False)
