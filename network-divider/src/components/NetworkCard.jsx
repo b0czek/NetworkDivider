34e91be75fa17bfb33544df00e91f36a55f04928
b0czek <dariusz.majnert@gmail.com>
2021-04-18 23:10:16 +0200

    added error card and tweaks

--- network-divider/src/components/NetworkCard.jsx
+++ network-divider/src/components/NetworkCard.jsx
@@ -7,8 +7,8 @@ const BinaryAddress = (props) => {
         props.binAddress.substring(props.prefix),
     ];
     return (
-        <span>
-            <span className="bin-network-part">{parts[0]}</span>
+        <span className={props.className}>
+            <span>{parts[0]}</span>
             <span className="bin-host-part">{parts[1]}</span>
         </span>
     );
@@ -16,86 +16,123 @@ const BinaryAddress = (props) => {
 
 const BinaryAddressContainer = (props) => {
     return (
-        <div className="network-address-bin col-12 col-lg-6">
+        <div className="col-12 col-lg-6">
             <BinaryAddress {...props} />
         </div>
     );
 };
 
 const DotDecimalAddress = (props) => (
-    <div className="network-address col-12 col-lg-3">
+    <div className="col-12 col-lg-3">
         {props.prefix ? `${props.address}=/${props.prefix}` : props.address}
     </div>
 );
+
 // dont include colon if there is no name
 const AddressRow = forwardRef((props, ref) => (
-    <Row ref={ref}>
-        <div className="col-12 col-lg-3">
-            {props.name}
-            {props.name ? ":" : null}
+    <Row ref={ref} className={props.className}>
+        <div
+            className={`col-12 col-lg-3 ${
+                props.nameColor ? "text-" + props.nameColor : null
+            }`}>
+            {props.name ? `${props.name}:` : null}
         </div>
         {props.children}
     </Row>
 ));
 
-const NetworkCard = (props) => {
-    return (
-        <Card>
+const CardContainer = forwardRef((props, ref) => (
+    <div className="col-12 col-md-10 mx-auto my-2">
+        <Card border={props.variant} className="thicken-border">
             <Card.Header>{props.header}</Card.Header>
-            <Card.Body>
+            <Card.Body className={`text-${props.variant}`}>
                 <Card.Title>{props.title}</Card.Title>
-                <Container>
-                    <AddressRow name="Adres sieci">
-                        <DotDecimalAddress address={props.data.network_address} />
-                        <BinaryAddressContainer
-                            binAddress={props.data.network_address_bin}
-                            prefix={props.data.subnet_mask_prefix}
-                        />
-                    </AddressRow>
-                    <AddressRow name="Maska podsieci">
-                        <DotDecimalAddress
-                            address={props.data.subnet_mask}
-                            prefix={props.data.subnet_mask_prefix}
-                        />
-                        <BinaryAddressContainer
-                            binAddress={props.data.subnet_mask_bin}
-                            prefix={props.data.subnet_mask_prefix}
-                        />
-                    </AddressRow>
-                    <AddressRow name="Adres rozgłoszeniowy">
-                        <DotDecimalAddress address={props.data.broadcast_address} />
-                        <BinaryAddressContainer
-                            binAddress={props.data.broadcast_address_bin}
-                            prefix={props.data.subnet_mask_prefix}
-                        />
-                    </AddressRow>
-                    <AddressRow name="Zakres adresów hostów">
-                        <div class="col-12 col-lg-6">
-                            <span class="first-host-address">{props.data.first_host_address}</span>
-                            <span class="separator">-</span>
-                            <span class="last-host-address">{props.data.last_host_address}</span>
-                        </div>
-                    </AddressRow>
-                    <AddressRow>
-                        <div class="col-12 col-lg-9">
-                            <BinaryAddress
-                                binAddress={props.data.first_host_address_bin}
-                                prefix={props.data.subnet_mask_prefix}
-                            />
-                            <span class="separator">-</span>
-                            <BinaryAddress
-                                binAddress={props.data.last_host_address_bin}
-                                prefix={props.data.subnet_mask_prefix}
-                            />
-                        </div>
-                    </AddressRow>
-                    <AddressRow name="Adresów w sieci">
-                        <div className="col-12 col-lg-9">{props.data.addresses_count}</div>
-                    </AddressRow>
-                </Container>
+                <Container ref={ref}>{props.children}</Container>
             </Card.Body>
         </Card>
+    </div>
+));
+
+const NetworkCard = (props) => {
+    return (
+        <CardContainer
+            variant={props.variant}
+            header={props.header}
+            title={props.title}>
+            <AddressRow
+                name="Adres sieci"
+                className="network-address"
+                nameColor={props.variant}>
+                <DotDecimalAddress address={props.data.network_address} />
+                <BinaryAddressContainer
+                    binAddress={props.data.network_address_bin}
+                    prefix={props.data.subnet_mask_prefix}
+                />
+            </AddressRow>
+            <AddressRow
+                name="Maska podsieci"
+                className="subnet-mask"
+                nameColor={props.variant}>
+                <DotDecimalAddress
+                    address={props.data.subnet_mask}
+                    prefix={props.data.subnet_mask_prefix}
+                />
+                <BinaryAddressContainer
+                    binAddress={props.data.subnet_mask_bin}
+                    prefix={props.data.subnet_mask_prefix}
+                />
+            </AddressRow>
+            <AddressRow
+                name="Adres rozgłoszeniowy"
+                className="network-address"
+                nameColor={props.variant}>
+                <DotDecimalAddress address={props.data.broadcast_address} />
+                <BinaryAddressContainer
+                    binAddress={props.data.broadcast_address_bin}
+                    prefix={props.data.subnet_mask_prefix}
+                />
+            </AddressRow>
+            <AddressRow name="Zakres adresów hostów">
+                <div className="col-12 col-lg-6">
+                    <span className="first-host">
+                        {props.data.first_host_address}
+                    </span>
+                    <span className="separator">-</span>
+                    <span className="last-host">
+                        {props.data.last_host_address}
+                    </span>
+                </div>
+            </AddressRow>
+            <AddressRow>
+                <div className="col-12 col-lg-9">
+                    <BinaryAddress
+                        binAddress={props.data.first_host_address_bin}
+                        prefix={props.data.subnet_mask_prefix}
+                        className="first-host"
+                    />
+                    <span className="separator">-</span>
+                    <BinaryAddress
+                        binAddress={props.data.last_host_address_bin}
+                        prefix={props.data.subnet_mask_prefix}
+                        className="last-host"
+                    />
+                </div>
+            </AddressRow>
+            <AddressRow name="Adresów w sieci">
+                <div className="col-12 col-lg-9">
+                    <span className="addresses-count">
+                        {props.data.addresses_count}
+                    </span>
+                </div>
+            </AddressRow>
+        </CardContainer>
     );
 };
+
+export const ErrorCard = (props) => (
+    <CardContainer variant="danger" header={props.header} title={props.title}>
+        <Row>{props.data.error}</Row>
+    </CardContainer>
+);
 
 export default NetworkCard;
